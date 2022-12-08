a771e80e63a2d8651a65d55f6472db8ec1e615da
Stephane AKOLATSE <akolatse1@gmail.com>
2022-12-08 15:43:00 -0800

    4.Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,8 @@
+#!/bin/bash
+#include <stdio.h>
+
+int main(void){
+	char message = "Programming is like building a multilingual puzzle\n";
+	puts(message);
+	return 0;
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This is the directory that contains the first tasks of c programming 
