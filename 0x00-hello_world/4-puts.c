9f5c28173cf722af833db4e7128c6c7d372ede7c
Stephane AKOLATSE <akolatse1@gmail.com>
2022-12-08 16:06:39 -0800

    4. Final fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	char message[] = "Programming is like building a multilingual puzzle\n";
+	char message[] = "\"Programming is like building a multilingual puzzle\n";
 
 	puts(message);
 	return (0);
