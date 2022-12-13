da688393c4805b92443674a5fb6866dac00eeed7
Stephane AKOLATSE <akolatse1@gmail.com>
2022-12-13 15:22:07 -0800

    Task 5...

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - check the site of an int
+ * @n: int to check
+ *
+ * Return: Always int
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (+1);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		printf("0");
+		return (0);
+	}
+
+}
