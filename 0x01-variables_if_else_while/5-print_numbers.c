2a209a2782d9fea3e4e57b1775ec68dd79d55b2c
Stephane AKOLATSE <akolatse1@gmail.com>
2022-12-12 18:31:08 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	for (int numb = 0 ; numb < 10 ; numb++)
+		printf("%d", numb);
+	putchar('\n');  
+	return (0);
+}
