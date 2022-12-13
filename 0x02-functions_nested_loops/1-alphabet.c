f973a6289326792cb7890f949186425af24a4af6
Stephane AKOLATSE <akolatse1@gmail.com>
2022-12-13 13:31:36 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+}
