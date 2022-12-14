bdb8fd1892e20a2ebaf829acd7a5ef37247d0ee0
Stephane AKOLATSE <akolatse1@gmail.com>
2022-12-13 16:07:02 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - A function that prints the lasts digit of an int
+ * @a: the int
+ *
+ * Return: Always int
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 2);
+}
